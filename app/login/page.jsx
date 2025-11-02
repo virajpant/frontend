95d36be6ba6fe3d89ac57b1299ff8df6bced5ea8
virajpant <vpant9211@gmail.com>
2025-11-02 15:42:19 +0530

    update login API endpoint to use production URL

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -9,7 +9,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/auth/login", {
+      const res = await fetch("https://backend-sigma-blush.vercel.app/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include", 
